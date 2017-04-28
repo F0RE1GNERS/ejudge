3a47be0daa1c274919911e98f213467c3e3cab08
ultmaster <scottyugochang@hotmail.com>
2017-04-28 08:27:38 +0800

    fix: core number and connection number

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ `id -u` -ne 0 ]; then
 fi
 
 core=$(grep --count ^processor /proc/cpuinfo)
-n=$(($core*4))
+n=$(($core*2))
 redis-server &
 celery worker -A config.celery &
-gunicorn server:app --workers $n --worker-connections 200 --error-logfile /var/log/gunicorn.log --timeout 3600 --graceful-timeout 3600 --worker-class gevent --bind 0.0.0.0:4999
+gunicorn server:app --workers $n --worker-connections 1000 --error-logfile /var/log/gunicorn.log --timeout 3600 --graceful-timeout 3600 --worker-class gevent --bind 0.0.0.0:4999
