e07b08b45182531af8501bfa6be124a5e11cc8a6
Scott Chang <scottyugochang@hotmail.com>
2017-05-04 22:22:46 +0800

    Update languages.py

--- core/languages.py
+++ core/languages.py
@@ -1,5 +1,7 @@
 from config import *
 
+_DEFAULT_ENV = ["LANG=en_US.UTF-8", "LANGUAGE=en_US:en", "LC_ALL=en_US.UTF-8"]
+
 LANGUAGE_SETTINGS = {
     'c': {
         "src_name": "main.c",
@@ -7,8 +9,8 @@ LANGUAGE_SETTINGS = {
         "max_memory": 128 * 1024 * 1024,
         "compile_cmd": "/usr/bin/gcc -DONLINE_JUDGE -O2 -w -std=c99 {src_path} -lm -o {exe_path}",
         "exe_cmd": "{exe_path}",
-        "seccomp_rule": "general",
-        "env": []
+        "seccomp_rule": "c_cpp",
+        "env": [] + _DEFAULT_ENV
     },
     'cpp': {
         "src_name": "main.cpp",
@@ -16,8 +18,8 @@ LANGUAGE_SETTINGS = {
         "max_memory": 128 * 1024 * 1024,
         "compile_cmd": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++11 {src_path} -lm -o {exe_path}",
         "exe_cmd": "{exe_path}",
-        "seccomp_rule": "general",
-        "env": []
+        "seccomp_rule": "c_cpp",
+        "env": [] + _DEFAULT_ENV
     },
     'java': {
         "src_name": "Main.java",
@@ -25,9 +27,9 @@ LANGUAGE_SETTINGS = {
         "max_memory": -1,
         "compile_cmd": "/usr/bin/javac {src_path} -encoding UTF8",
         "exe_cmd": "/usr/bin/java -cp {exe_dir} -Xss1M -XX:MaxPermSize=16M -XX:PermSize=8M -Xms16M -Xmx{max_memory}M "
-                   "-Djava.security.manager -Djava.security.policy==/etc/java_policy -Djava.awt.headless=true {exe_name}",
+                   "-Djava.security.manager -Dfile.encoding=UTF-8 -Djava.security.policy==/etc/java_policy -Djava.awt.headless=true {exe_name}",
         "seccomp_rule": None,
-        "env": ["MALLOC_ARENA_MAX=1"]
+        "env": ["MALLOC_ARENA_MAX=1"] + _DEFAULT_ENV
     },
     'python': {
         # A Naive solution of copy
@@ -37,6 +39,6 @@ LANGUAGE_SETTINGS = {
         "compile_cmd": "/usr/bin/python3 -m py_compile {src_path}",
         "exe_cmd": "/usr/bin/python3 {exe_path}",
         "seccomp_rule": "general",
-        "env": ["LANG=en_US.UTF-8"]
+        "env": [] + _DEFAULT_ENV
     }
 }
