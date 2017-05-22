83f05db32c610167463a7a5608d61f8602d09424
Scott Chang <scottyugochang@hotmail.com>
2017-05-22 22:17:23 +0800

    DO NOT USE IT! memory in time, ridiculous...

--- judger/src/runner.c
+++ judger/src/runner.c
@@ -132,11 +132,8 @@ void run(struct config *_config, struct result *_result) {
         }
 
         _result->exit_code = WEXITSTATUS(status);
-        _result->cpu_time = (int) (resource_usage.ru_utime.tv_sec * 1000 +
-                                  resource_usage.ru_utime.tv_usec / 1000 +
-                                  resource_usage.ru_stime.tv_sec * 1000 +
-                                  resource_usage.ru_stime.tv_usec / 1000);
-        _result->memory = memory_usage * 1024;
+        _result->cpu_time = (int) (resource_usage.ru_utime.tv_sec * 1000 + resource_usage.ru_utime.tv_usec / 1000);
+        _result->memory = (int) ((resource_usage.ru_stime.tv_sec * 1000 + resource_usage.ru_stime.tv_usec / 1000) * 1000);
 
         // get end time
         gettimeofday(&end, NULL);
